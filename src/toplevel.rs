0c2c792c06794346691acf6a246e18bba27b3978
Finomnis <finomnis@gmail.com>
2023-10-08 21:59:42 +0200

    Example 06

--- src/toplevel.rs
+++ src/toplevel.rs
@@ -89,7 +89,7 @@ impl<ErrType: ErrTypeTraits> Toplevel<ErrType> {
                 return result;
             },
             _ = self.root_handle.on_shutdown_requested() => {
-                tracing::info!("Initiating shutdown ...");
+                tracing::info!("Shutting down ...");
             }
         );
 
